6a6141194cad3931a5c6b59aef4c75ec3becdf8c
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-10-29 14:47:14 +0700

    add tests to verify the merkle root functionality

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -1,34 +1,21 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract Whitelist {
+    bytes32 public merkleRoot;
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    constructor(bytes32 _merkleRoot) {
+        merkleRoot = _merkleRoot;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function checkInWhitelist(
+        bytes32[] calldata proof,
+        uint64 maxAllowanceToMint
+    ) public view returns (bool) {
+        bytes32 leaf = keccak256(abi.encode(msg.sender, maxAllowanceToMint));
+        bool verified = MerkleProof.verify(proof, merkleRoot, leaf);
+        return verified;
     }
 }
